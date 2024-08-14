0598018d0a0913f021a0e8898544b93c8f5a60dc
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-08-14 09:18:41 +0700

    mongoose conn

--- server/app.js
+++ server/app.js
@@ -1,4 +1,9 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
 const port = 3000;
 import authRouter from "./routes/authRouter.js";
@@ -9,4 +14,9 @@ app.use("/api/v1/auth", authRouter);
 // server
 app.listen(port, () => {
   console.info(`Server running on port ${port}`);
+});
+
+// conn
+mongoose.connect(process.env.DATABASE, {}).then(() => {
+  console.info("Database connected");
 });
