4a2c79156e63660362a8408201cc506cf1d172dd
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-08-15 12:29:11 +0700

    userModel

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,20 +1,39 @@
 // mongoose => schema
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import validator from "validator";
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
   name: {
     type: String,
     required: [true, "Please add a name"],
+    unique: [true, "Name already exists"],
   }, // String is shorthand for {type: String}
   email: {
     type: String,
     required: [true, "Please add an email"],
+    unique: [true, "Email already registered"],
+    validate: {
+      validator: validator.isEmail,
+      message: "it's not valid email",
+    },
   },
   password: {
     type: String,
     required: [true, "Please add a password"],
+    minLength: [6, "Password must be at least 6 characters"],
   },
+  role_id: {
+    type: String,
+    enum: ["user", "owner"],
+    default: "user",
+  },
+});
+
+userSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 const User = mongoose.model("User", userSchema);
