cfee23a7dad72e69ffcefce1e598a4c0fa126c1f
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-08-25 12:42:23 +0700

    orderItem in orderController

--- server/controller/OrderController.js
+++ server/controller/OrderController.js
@@ -1,7 +1,29 @@
 import asyncHandler from "../middleware/asyncHandler.js";
+import Product from "../models/productModel.js";
+import Order from "../models/orderModel.js";
 
 export const CreateOrder = asyncHandler(async (req, res) => {
+  const { firstName, lastName, email, phone, cartItem } = req.body;
+
+  if (!cartItem || cartItem.length > 0) {
+    res.status(400);
+    throw new Error("Cart is empty");
+  }
+
+  let orderItem = [];
+  let totalHarga = 0;
+
+  for (const cart of cartItem) {
+    const productData = await Product.findOne({ _id: cart.product });
+    if (!productData) {
+      res.status(404);
+      throw new Error("ID Product Not Found");
+    }
+    orderItem = [...orderItem, productData];
+  }
+
   res.status(201).json({
+    orderItem,
     message: "Create Order Success",
   });
 });
