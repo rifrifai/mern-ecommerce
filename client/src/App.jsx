a8c32bd11b6c8286d12a9d00d26b9e7249ac2075
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-08-26 23:39:24 +0700

    createBrowserRouter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,53 @@
 /* eslint-disable no-unused-vars */
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+// component
+import AboutView from "./page/AboutView";
+import CartView from "./page/CartView";
+import HomeView from "./page/HomeView";
+import OrderView from "./page/OrderView";
+import ProductView from "./page/ProductView";
+import LoginView from "./page/auth/LoginView";
+import RegisterView from "./page/auth/RegisterView";
+import PublicLayout from "./layouts/PublicLayout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: PublicLayout,
+    children: [
+      {
+        index: true,
+        element: <HomeView />,
+      },
+      {
+        path: "products",
+        element: <ProductView />,
+      },
+      {
+        path: "about",
+        element: <AboutView />,
+      },
+      {
+        path: "cart",
+        element: <CartView />,
+      },
+      {
+        path: "orders",
+        element: <OrderView />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <LoginView />,
+  },
+  {
+    path: "/register",
+    element: <RegisterView />,
+  },
+]);
+
 function App() {
   return (
     <>
