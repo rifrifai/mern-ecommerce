796545ffa3012871a59413e61eca4f7a57274196
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-08-27 15:12:31 +0700

    conn backend and frontend success

--- client/src/page/HomeView.jsx
+++ client/src/page/HomeView.jsx
@@ -1,3 +1,12 @@
+import axios from "axios";
+
+try {
+  const data = await axios.get("api/v1/product");
+  console.info(data);
+} catch (error) {
+  console.info(error);
+}
+
 const HomeView = () => {
   return <div>HomeView</div>;
 };
