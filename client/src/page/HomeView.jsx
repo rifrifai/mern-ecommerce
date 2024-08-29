796de9c72377c34e428d0131ec7975aedacaa40f
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-08-29 16:02:27 +0700

    get all product to fe success, but image can't be displayed

--- client/src/page/HomeView.jsx
+++ client/src/page/HomeView.jsx
@@ -1,14 +1,53 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable no-unused-vars */
 import axios from "axios";
-
-try {
-  const data = await axios.get("api/v1/product");
-  console.info(data);
-} catch (error) {
-  console.info(error);
-}
+import customApi from "../api.js";
+import { useState, useEffect } from "react";
 
 const HomeView = () => {
-  return <div>HomeView</div>;
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    try {
+      const { data } = await customApi.get("/product");
+      setProducts(data.data);
+      console.log(data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  return (
+    <>
+      <div className="border-b border-primary pb-2">
+        <h2 className="text-2xl font-bold lowercase"> product list</h2>
+      </div>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {products.map((i) => (
+          <div
+            className="card bg-neutral text-neutral-content shadow-xl"
+            key={i._id}
+          >
+            <figure>
+              <img src={i.image} alt="Shoes" />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{i.name}</h2>
+              <p className="font-bold text-primary">Rp. {i.price}</p>
+              <p>{i.description.substring(0, 50)}</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default HomeView;
