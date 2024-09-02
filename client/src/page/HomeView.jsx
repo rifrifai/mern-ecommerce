0be5d870ab2ce1e6ce641d7368593ae4e32a9cd7
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-09-02 09:49:42 +0700

    format price and limit 4 products

--- client/src/page/HomeView.jsx
+++ client/src/page/HomeView.jsx
@@ -9,12 +9,21 @@ const HomeView = () => {
 
   const getProducts = async () => {
     try {
-      const { data } = await customApi.get("/product");
+      const { data } = await customApi.get("/product?limit=4");
       setProducts(data.data);
       console.log(data.data);
     } catch (error) {
       console.log(error);
     }
+  };
+
+  // format harga rupiah
+  const priceFormat = (price) => {
+    const rupiah = new Intl.NumberFormat("id-ID", {
+      style: "currency",
+      currency: "IDR",
+    }).format(price);
+    return rupiah;
   };
 
   useEffect(() => {
@@ -24,9 +33,9 @@ const HomeView = () => {
   return (
     <>
       <div className="border-b border-primary pb-2">
-        <h2 className="text-2xl font-bold lowercase"> product list</h2>
+        <h2 className="text-2xl font-bold capitalize"> product list</h2>
       </div>
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 mt-5">
         {products.map((i) => (
           <div
             className="card bg-neutral text-neutral-content shadow-xl"
@@ -37,7 +46,7 @@ const HomeView = () => {
             </figure>
             <div className="card-body">
               <h2 className="card-title">{i.name}</h2>
-              <p className="font-bold text-primary">Rp. {i.price}</p>
+              <p className="font-bold text-primary">{priceFormat(i.price)}</p>
               <p>{i.description.substring(0, 50)}</p>
               <div className="card-actions justify-end">
                 <button className="btn btn-primary">Buy Now</button>
