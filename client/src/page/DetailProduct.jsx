e060c8ab4cd37dd75034f7a71b942b6d05ba2f07
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-09-03 11:03:04 +0700

    detailProduct success

--- client/src/page/DetailProduct.jsx
+++ client/src/page/DetailProduct.jsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import customApi from "../api";
 import { useEffect, useState } from "react";
+import { FaPlus } from "react-icons/fa6";
 
 export default function DetailProduct() {
   let { id } = useParams();
@@ -25,18 +26,26 @@ export default function DetailProduct() {
 
   return (
     <section>
-      <div className="card card-side bg-neutral-300 shadow-xl">
+      <div className="card lg:card-side bg-base-300 shadow-xl">
         <figure>
-          <img src={product.image} alt={product.name} />
+          <img
+            src={product.image}
+            alt={product.name}
+            className="w-[400px] h-[500px] object-cover"
+          />
         </figure>
         <div className="card-body">
           <h2 className="card-title">{product.name}</h2>
           <span className="text-3xl font-bold text-accent-content">
             {formatPrice(product.price)}
           </span>
+          <span className="badge badge-primary">{product.category}</span>
+          <span className="mt-3 font-bold">Stok: {product.stock}</span>
           <p className="mt-3">{product.description}</p>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy</button>
+            <button className="btn btn-primary btn-lg">
+              <FaPlus /> Keranjang
+            </button>
           </div>
         </div>
       </div>
