14b9da72f9afba591ee187d2cc236b604eac4b68
R. Abdurrohman <rifaiabdurrohman8@gmail.com>
2024-09-05 09:05:58 +0700

    tampilan product and filter success

--- client/src/page/ProductView.jsx
+++ client/src/page/ProductView.jsx
@@ -1,6 +1,7 @@
 import { useLoaderData } from "react-router-dom";
 import customApi from "../api";
 import Filter from "../components/Filter";
+import CartProduct from "../components/CartProduct";
 
 export const loader = async ({ request }) => {
   const { data } = await customApi.get("/product");
@@ -11,11 +12,15 @@ export const loader = async ({ request }) => {
 };
 const ProductView = () => {
   const { products } = useLoaderData();
-  console.log(products);
 
   return (
     <>
       <Filter />
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-5">
+        {products.map((i) => (
+          <CartProduct key={i._id} i={i} />
+        ))}
+      </div>
     </>
   );
 };
